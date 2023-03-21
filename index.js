f0c4c2470677e8bd2dcb30b71a942540c74fb753
yasar2385 <61082958+yasar2385@users.noreply.github.com>
2023-03-21 13:16:30 +0530

    1 file modified

--- index.js
+++ index.js
@@ -48,12 +48,36 @@ console.log(arr.swapItems(3, 7));
 document.querySelectorAll('.btnAuGroup').forEach((elm, idx, arr) => {
   elm.onclick = function (e) {
     console.log(this.id);
-    var arr = Array.from(this.parentElement.children);
-    var a = Array.prototype.indexOf.call(arr, this);
-    if (this.id.match(/up|down/) && a == 0) {
-      console.log(a);
-      let b = arr[this.id.match(/up/) ? idx - 1 : idx + 1];
-      arr[a] = arr.splice(b, 1, this[a])[0];
+    var el = this.closest('.form-group');
+    var arr = Array.from(el.parentElement.children);
+    var a = Array.prototype.indexOf.call(arr, el);
+    //console.log([el, arr, a]);
+    var [txt1, txt2] = ['', ''];
+    arr.forEach((e, id, a) => {
+      txt1 += e.querySelector('input').value;
+    });
+    console.log(txt1);
+    if (this.id.match(/up|down/)) {
+      let b = this.id.match(/up/) ? a - 1 : a + 1;
+      console.log([a, b]);
+      arr[a] = arr.splice(b, 1, arr[a])[0];
     }
+    arr.forEach((e, id, a) => {
+      txt2 += e.querySelector('input').value;
+    });
+    console.log(txt2);
   };
 });
+
+function dedmo(a, b) {
+  let collectionName = Array.from(preview.querySelectorAll('.string-name'));
+  console.log(collectionName);
+  let before = collectionName[a].querySelector('.surname').textContent;
+  let CloneArray = Array.from(collectionName).map((el) => el.cloneNode(true));
+  collectionName[a].replaceWith(CloneArray[b]);
+  collectionName[b].replaceWith(CloneArray[a]);
+  let after = collectionName[a].querySelector('.surname').textContent;
+  console.log([after, before]);
+}
+
+dedmo(4, 1);
